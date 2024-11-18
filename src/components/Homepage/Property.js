d29cfd7aa59afb4be8665e79db5ffccae0c90381
Snowkids-code <oumaj47@gmail.com>
2024-11-18 08:51:15 +0300

    add property cards

--- src/components/Homepage/Property.js
+++ src/components/Homepage/Property.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
+import PropertyCard from "./PropertyCard";
 
-export default function Property() {
+export default function Property({ property }) {
   return (
-    <div className='property-container'>
-        <div className='property-wrapper'>
-            <div className='property-header'>
-                <h3>Explore Our Hot Deals</h3>
-            </div>
-            <div className='property-body'></div>
+    <div className="property-container">
+      <div className="property-wrapper">
+        <div className="property-header">
+          <h3>Explore Our Hot Deals</h3>
         </div>
+        {property && (
+          <div className="property-body">
+            {property.data?.map((value, i) => (
+              <PropertyCard details={value} />
+            ))}
+          </div>
+        )}
+        
+      </div>
     </div>
-  )
+  );
 }
