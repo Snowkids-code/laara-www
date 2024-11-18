eba72610e62f346e6d8c3f83dad3cc5aae5105b2
Snowkids-code <oumaj47@gmail.com>
2024-11-18 06:57:01 +0300

    create destination component

---
+++ src/components/Homepage/Destinations.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Destinations() {
+  return (
+    <div className='destinations-container'>
+        <div className='destinations-wrapper'>
+            <div className='destinations-header'>
+                <h3>Explore Our Hot Deals</h3>
+            </div>
+            <div className='destinations-body'></div>
+        </div>
+    </div>
+  )
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Cover from "../components/Homepage/Cover";
+import Destinations from "../components/Homepage/Destinations";
 
 export default function Homepage() {
   return (
     <div>
       <Cover />
+      <Destinations />
     </div>
   );
 }
